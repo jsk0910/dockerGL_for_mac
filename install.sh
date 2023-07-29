d3379644d06a962a464268078cfbe1c69ccfd872
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-30 02:47:01 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,4 +9,4 @@ DISPLAY=:0
 sudo xhost +
 
 docker build --tag macosopengl:1.0 .
-sudo docker run --rm -it -v ./src:/src -e DISPLAY=121.174.207.215:0  --net=host -p 6006:6006 -p 8888:8888 --privileged --name="macosOpenGL" macosopengl:1.0
+sudo docker run --rm -it -v ./src:/src -e DISPLAY=X.X.X.X:0  --net=host -p 6006:6006 -p 8888:8888 --privileged --name="macosOpenGL" macosopengl:1.0
